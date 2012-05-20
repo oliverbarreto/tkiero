304bf0b426a56c6fde668bb1953e5c172d4a12aa
Oliver <oliver.barreto.online@gmail.com>
2012-05-20 17:32:30 +0100

    Fixed some Tests failures

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,33 @@
+#User Pages = Handles Sign Up Form
 require 'spec_helper'
 
-describe "UserPages" do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get user_pages_index_path
-      response.status.should be(200)
-    end
+describe "User pages" do
+  let(:base_title) { "tKiero App" }
+  subject { page }
+
+  # Page Content 
+  describe "Signup page" do
+
+    before { visit signup_path }
+
+    it { should have_content('Build One IDeas') }
+    it { should have_selector('h2',    text: "#{base_title}") }
+    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector 'title', text: '| Sign Up' }
   end
+
+  # Page Content
+  describe "Profile page" do
+
+    # Code to make a user variable from ActiveRecord:Test:Environment with FactoryGirl gem 
+    let(:user) { FactoryGirl.create(:user) }
+
+    before { visit user_path(user) }
+
+    it { should have_content('Build One IDeas') }
+    it { should have_selector('h2',    text: user.name) }
+    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector 'title', text: "| #{user.name}" }
+  end
+
 end
