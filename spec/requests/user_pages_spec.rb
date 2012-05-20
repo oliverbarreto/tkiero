6626a9c95dc5f9bd01c72f4f4f9342ecbf893438
Oliver <oliver.barreto.online@gmail.com>
2012-05-20 19:29:33 +0100

    updated Custom.CSS to add a red line around invalid form textfields

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,11 +9,37 @@ describe "User pages" do
   describe "Signup page" do
 
     before { visit signup_path }
+    let(:submit) { "Create your account..." }
+  
+    describe "Singup page correct content" do
+      it { should have_content('Build One IDeas') }
+      it { should have_selector('h2',    text: "#{base_title}") }
+      it { should have_selector('title', text: full_title('')) }
+      it { should have_selector 'title', text: '| Sign Up' }
+    end
+    
+    # Test Invalid information - Send an empty form
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
 
-    it { should have_content('Build One IDeas') }
-    it { should have_selector('h2',    text: "#{base_title}") }
-    it { should have_selector('title', text: full_title('')) }
-    it { should have_selector 'title', text: '| Sign Up' }
+    # Test Valid information - Send an correct form
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+    
+    
   end
 
   # Page Content
